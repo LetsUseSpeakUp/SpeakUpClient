3639d762194690622f603618d1110b2fa68d9601
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-05-23 16:18:05 -0700

    Added contacts.

--- App/ContactsScreen/Logic/useContacts.tsx
+++ App/ContactsScreen/Logic/useContacts.tsx
@@ -1,16 +1,35 @@
-import * as Contacts from 'react-native-contacts'
-import {useState, useEffect} from 'react'
+import * as Contacts from 'react-native-contacts';
+import React, {useState, useEffect} from 'react'
 
-export default function useContacts(){
-    const [contacts, setContacts] = useState([]);
+/**
+ * 
+ * @returns Contact data from iOS/Android
+ * This will probably have errors on Web ?
+ */
+function useContactData(){
+    const [contactData, setContactData] = useState([]);
 
-    useEffect(()=>{
-        Contacts.getAllWithoutPhotos().then((fetchedContacts)=>{
-            setContacts(fetchedContacts);
-        }).catch((error)=>{
-            console.log("useContacts.tsx -- failed to get contacts: ", error)
-        })        
-    }, [])
+    React.useEffect(()=>{
+        fetchContactData();
+    }, [])    
 
-    return contacts;
+    function fetchContactData(){
+        Contacts.getAll().then(fetchedContacts=>{
+            fetchedContacts = fetchedContacts.filter(contact => contact.phoneNumbers.length > 0);
+            fetchedContacts.sort((a, b) => {
+                const name1 = a.familyName || a.givenName;
+                const name2 = b.familyName || b.givenName;
+                return name1.localeCompare(name2);
+            })
+            console.log("ContactsLogic.js::received contacts data. Now updating");
+            setContactData(fetchedContacts)
+        }).catch(()=>{        
+            console.log("ContactsLogic::Phone didn't allow contacts") 
+            //TODO: Set contactData to an error state and handle that on caller. Just using length == 0 is fine for now though
+        })
+    }
+
+    return contactData;
 }
+
+export {useContactData}
--- App/ContactsScreen/index.tsx
+++ App/ContactsScreen/index.tsx
@@ -1,14 +1,81 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import useContacts from './Logic/useContacts'
+import * as React from 'react';
 
-export default function ContactsScreen() {
-  const contacts = useContacts();
-  console.log("ContactsScreen.tsx -- contacts size: ", contacts.length);
+import {useContactData} from './Logic/useContacts'
+import {Text, View, FlatList, StyleSheet, Button} from 'react-native';
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Convos!</Text>
-    </View>
-  );
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 22,
+    },
+    item: {
+      paddingHorizontal: 8,
+      paddingVertical: 8,
+      marginHorizontal: 20,
+      borderBottomColor: '#CCCCCC',
+      borderBottomWidth: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    itemText: {
+      fontSize: 16,
+    },
+    message: {
+        paddingHorizontal: 8,
+        paddingVertical: 100,        
+        borderBottomColor: '#CCCCCC',
+        borderBottomWidth: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    }
+  });
+
+export default function ContactsScreen(){
+    const contactsData = useContactData(); 
+    
+    console.log('Contacts.js::Got contacts. Contacts length:', contactsData.length);    
+
+    if(contactsData.length == 0)
+        return EmptyContactsScreen();
+    else return(
+        <View style = {styles.container}>
+            <ContactsFlatList contactsData={contactsData}></ContactsFlatList>                    
+        </View>
+    )
 }
+
+function EmptyContactsScreen(){
+    return(
+        <View style = {styles.message}>
+            <Text>Please allow SpeakUps to access contacts in your phone settings</Text>
+        </View>        
+    )
+}
+
+function ContactsFlatList({contactsData} : any){
+    let flatListData = []
+    if(contactsData.length > 0)
+        flatListData = contactsData.map((c: any)=>{return {key: c.recordID, value: c}})    
+
+    return(
+        <FlatList 
+            data={flatListData}
+            renderItem ={({item})=> <SingleContactItem contact={item.value}/>}
+        />
+    )
+}
+
+function SingleContactItem({contact} : any){
+    return(
+        <View style={styles.item}>
+            <Text style={styles.itemText}>
+                {`${contact.givenName} ${contact.familyName}`}
+            </Text>
+        </View>
+    )
+}
+
+export {ContactsScreen}
