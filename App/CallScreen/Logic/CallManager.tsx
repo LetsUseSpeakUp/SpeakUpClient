0204b011e0c570171b593c53d8194c2f31159670
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-05-24 18:07:54 -0700

    Worked a little on joining agora channel.

--- App/CallScreen/Logic/CallManager.tsx
+++ App/CallScreen/Logic/CallManager.tsx
@@ -15,7 +15,6 @@ import { SignalServer, MessageType, SignalServerData } from './SignalServer'
 class CallManager extends EventEmitter {
 
     signalServer: SignalServer
-    //@ts-ignore
     myPhoneNumber: string
     partnerPhoneNumber: string
     tempAgoraChannel: string
@@ -30,24 +29,32 @@ class CallManager extends EventEmitter {
         this.setupSignalServer(myPhoneNumber);
     }
 
-    public placeCall(receiverPhoneNumber: string, agoraChannel: string) {
+    public placeCall(receiverPhoneNumber: string) {
+        const agoraChannelName = this.generateAgoraChannelName(this.myPhoneNumber);
         //TODO: Join Agora Channel
         this.partnerPhoneNumber = receiverPhoneNumber;
-        this.signalServer.sendSignal({agoraChannel: agoraChannel, myPhoneNumber: this.myPhoneNumber,
+        this.signalServer.sendSignal({agoraChannel: agoraChannelName, myPhoneNumber: this.myPhoneNumber,
             receiverPhoneNumber: this.partnerPhoneNumber});
     }
 
     public acceptCall() {
-        //TODO
-            //Join agora Channel
+        if(this.tempAgoraChannel.length == 0){
+            console.log("ERROR -- CallManager.tsx. Temp Agora Channel name is length 0");
+            return;
+        }
+        
+        this.joinAgoraChannel(this.tempAgoraChannel);
     }
 
     public declineCall() {        
         this.signalServer.sendDecline(this.myPhoneNumber, this.partnerPhoneNumber);
+        this.resetPartner();
     }
 
     public endCall() {
         //TODO
+        this.leaveAgoraChannel();
+        this.resetPartner();
     }
 
     private setupSignalServer = (myNumber: string) => {
@@ -66,7 +73,26 @@ class CallManager extends EventEmitter {
             //TODO
             //Leave Agora channel
             this.emit("callDeclined");
+            this.resetPartner();
         })
+    }
+
+    private joinAgoraChannel = (channelName: string)=>{
+        //TODO
+        //TODO: Put a timeout - if you don't connect to partner within 30 seconds, disconnect
+    }
+
+    private leaveAgoraChannel = ()=>{
+        //TODO
+    }
+
+    private generateAgoraChannelName = (myNumber: string):string=>{
+        return "TODOSETAGORACHANNEL"; //TODO: Put this in agoraManager
+    }
+
+    private resetPartner = ()=>{
+        this.tempAgoraChannel = "";
+        this.partnerPhoneNumber = "";
     }
 }
 
