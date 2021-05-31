fa8d3b6485ab87912bf502ceb618bd30330aa7c3
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-05-31 10:07:11 -0700

    We're getting somewhere with nav test, but not 100% working.

--- App/index.tsx
+++ App/index.tsx
@@ -24,6 +24,7 @@ export default function App() {
         console.log("ERROR -- App. Phone number was changed after fetching convosMetadata. New number: ", userPhoneNumber);
       }
       ConvosManager.fetchLatestConvosMetadataForUser(userPhoneNumber).then((metadata: any) => {
+        convoToNavToBuffer.current = metadata[0].convoId; //TODO: Just for testing. Delete this
         setConvosMetadata(metadata);
       })
     }
