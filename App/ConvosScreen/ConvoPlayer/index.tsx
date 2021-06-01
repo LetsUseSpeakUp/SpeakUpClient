ade3167ec419f6c26bda89d6fc67f8ad1b30e0ed
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-01 10:46:40 -0700

    Got slider working correctly.

--- App/ConvosScreen/ConvoPlayer/index.tsx
+++ App/ConvosScreen/ConvoPlayer/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {useState} from 'react';
 import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from "react-native";
 import { Slider } from 'react-native-elements'
@@ -13,13 +13,26 @@ export default function ConvoPlayer() {
 
 function ProgressSlider() {
     const [progress, setProgress] = useState(0)
+    const [slidingCompleteVal, setSlidingCompleteVal] = useState(0);
+
+    const onSlidingComplete = (value: number)=>{
+        console.log("ConvoPlayer. onSlidingComplete. Progress val: ", value);
+        setSlidingCompleteVal(value);
+    }
+
+    useEffect(()=>{
+        console.log("ConvoPlayer. progress changed. Progress: ", progress);
+    }, [progress])
+
     return (
         <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center', paddingHorizontal: 20 }}>
             <Slider
                 value={progress}
                 onValueChange={(value) => setProgress(value)}
+                onSlidingComplete={(value)=>{onSlidingComplete(value)}}
             />
             <Text>Value: {progress}</Text>
+            <Text>Sliding Complete: {slidingCompleteVal}</Text>
         </View>
     )
 }
