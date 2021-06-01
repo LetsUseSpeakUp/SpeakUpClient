8d68757609eee9a9af80ac2b11f73ef87d6e3dd6
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-01 13:34:38 -0700

    Now displaying info about track.

--- App/ConvosScreen/ConvoPlayer/index.tsx
+++ App/ConvosScreen/ConvoPlayer/index.tsx
@@ -3,24 +3,31 @@ import { useState } from 'react';
 import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, Button } from "react-native";
 import { Slider } from 'react-native-elements'
 import TrackPlayer from 'react-native-track-player';
+import {useTrackPlayerProgress} from 'react-native-track-player';
 
 export default function ConvoPlayer() {
+    const [isPlayerInitialized, setIsPlayerInitialized] = useState(false);
     const [playbackState, setPlaybackState] = useState(TrackPlayer.STATE_NONE);
+    const trackPlayerProgress = useTrackPlayerProgress(100);
+    const trackPosition = trackPlayerProgress.position;
+    const trackDuration = trackPlayerProgress.duration;
     // const [sliderProgress, setSliderProgress] = useState(0)
     // const [slidingCompleteVal, setSlidingCompleteVal] = useState(0);
 
     useEffect(() => {
-        initializeTrackPlayer();
+        initializeTrackPlayer().then(()=>{
+            setIsPlayerInitialized(true);
+        });
     }, []);
 
     const onPlayPauseButtonPressed= ()=>{
         if(playbackState === TrackPlayer.STATE_PLAYING){
             TrackPlayer.pause();
-            setPlaybackState(TrackPlayer.STATE_PLAYING);
+            setPlaybackState(TrackPlayer.STATE_PAUSED);
         }
         else{
             TrackPlayer.play();
-            setPlaybackState(TrackPlayer.STATE_PAUSED);
+            setPlaybackState(TrackPlayer.STATE_PLAYING);
         }
     }
 
@@ -32,7 +39,9 @@ export default function ConvoPlayer() {
                 // onSlidingComplete={(value) => { setSlidingCompleteVal(value) }}
                 thumbStyle={{ height: 30, width: 10 }}  
             />
-            <Button title={playbackState === TrackPlayer.STATE_PLAYING ? "Pause": "Play"} onPress={() => {onPlayPauseButtonPressed()}} />
+            <Text>Track Position: {trackPosition}</Text>
+            <Text>Track Duration: {trackDuration}</Text>
+            <Button title={playbackState === TrackPlayer.STATE_PLAYING ? "Pause": "Play"} onPress={() => {onPlayPauseButtonPressed()}} disabled={!isPlayerInitialized}/>
         </SafeAreaView>
 
     )
@@ -56,5 +65,5 @@ async function initializeTrackPlayer() { //TODO: Take convo details as param
         artist: 'artist1',
         artwork: 'https://picsum.photos/300'
     });
-    console.log("ConvoPlayer::InitializeTrackPlayer. Initialized");
+    console.log("ConvoPlayer::InitializeTrackPlayer. Initialized");    
 }
