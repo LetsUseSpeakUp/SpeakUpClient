7ca4f9dd052e9a60caf597d51b81aef2b54d0989
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-01 13:22:44 -0700

    Got audio playing working. Now need to implement pausing and wiring up to progress bar.

--- App/ConvosScreen/ConvoPlayer/index.tsx
+++ App/ConvosScreen/ConvoPlayer/index.tsx
@@ -1,38 +1,60 @@
 import React, { useEffect } from 'react';
-import {useState} from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, Button} from "react-native";
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, Button } from "react-native";
 import { Slider } from 'react-native-elements'
-
+import TrackPlayer from 'react-native-track-player';
 
 export default function ConvoPlayer() {
-    //TODO
+    const [playbackState, setPlaybackState] = useState(TrackPlayer.STATE_NONE);
+    // const [sliderProgress, setSliderProgress] = useState(0)
+    // const [slidingCompleteVal, setSlidingCompleteVal] = useState(0);
+
+    useEffect(() => {
+        initializeTrackPlayer();
+    }, []);
+
+    const onPlayPauseButtonPressed= ()=>{
+        if(playbackState === TrackPlayer.STATE_PLAYING){
+            TrackPlayer.pause();
+            setPlaybackState(TrackPlayer.STATE_PLAYING);
+        }
+        else{
+            TrackPlayer.play();
+            setPlaybackState(TrackPlayer.STATE_PAUSED);
+        }
+    }
+
     return (
         <SafeAreaView style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center', paddingHorizontal: 20 }}>
-            <ProgressSlider/>
-            <Button title="Play" onPress={()=>{}} />
-        </SafeAreaView>
-        
-    )
-}
-
-function ProgressSlider() {
-    const [progress, setProgress] = useState(0)
-    const [slidingCompleteVal, setSlidingCompleteVal] = useState(0);
-
-    const onSlidingComplete = (value: number)=>{        
-        setSlidingCompleteVal(value);
-    }    
-
-    return (
-        <View >
             <Slider
-                value={progress}
-                onValueChange={(value) => setProgress(value)}
-                onSlidingComplete={(value)=>{onSlidingComplete(value)}}
+                // value={sliderProgress}
+                // onValueChange={(value) => setSliderProgress(value)}
+                // onSlidingComplete={(value) => { setSlidingCompleteVal(value) }}
+                thumbStyle={{ height: 30, width: 10 }}  
             />
-            <Text>Value: {progress}</Text>
-            <Text>Sliding Complete: {slidingCompleteVal}</Text>
-        </View>
+            <Button title={playbackState === TrackPlayer.STATE_PLAYING ? "Pause": "Play"} onPress={() => {onPlayPauseButtonPressed()}} />
+        </SafeAreaView>
+
     )
 }
 
+
+async function initializeTrackPlayer() { //TODO: Take convo details as param
+    await TrackPlayer.setupPlayer();
+    TrackPlayer.updateOptions({
+        capabilities: [
+            TrackPlayer.CAPABILITY_PAUSE,
+            TrackPlayer.CAPABILITY_PLAY,
+        ]
+    })
+    await TrackPlayer.add({ //TODO: Update this
+        id: 'songID1',
+        url: 'https://en.wikipedia.org/wiki/File:Komm%27_o_Hoffnung.ogg',
+        type: 'default',
+        title: 'songTitle1',
+        album: 'album1',
+        artist: 'artist1',
+        artwork: 'https://picsum.photos/300'
+    });
+    console.log("ConvoPlayer::InitializeTrackPlayer. Initialized");
+}
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { registerRootComponent } from 'expo';
 import './shim.js'
 import App from './App';
+import TrackPlayer from 'react-native-track-player';
 
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+TrackPlayer.registerPlaybackService = require('./TrackPlayerService');
