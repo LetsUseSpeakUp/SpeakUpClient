8137d07041646a8aeeb8d000b0c7991d88355f26
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-01 10:29:48 -0700

    Added slider.

--- App/ConvosScreen/ConvoPlayer/index.tsx
+++ App/ConvosScreen/ConvoPlayer/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {useState} from 'react';
 import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from "react-native";
+import { Slider } from 'react-native-elements'
 
 
-export default function ConvoPlayer(){
+export default function ConvoPlayer() {
     //TODO
-    return(
-        <Text>CONVO Player. TODO</Text>
+    return (
+        <ProgressSlider/>
     )
 }
+
+function ProgressSlider() {
+    const [progress, setProgress] = useState(0)
+    return (
+        <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center', paddingHorizontal: 20 }}>
+            <Slider
+                value={progress}
+                onValueChange={(value) => setProgress(value)}
+            />
+            <Text>Value: {progress}</Text>
+        </View>
+    )
+}
+
