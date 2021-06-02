7e6e584553368799615eec2a4d94fec59a62e011
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-01 17:41:30 -0700

    Pause/play doesn't work perfectly from system pull down if you're already at the end of the track, but whatever. It's good enough for now.

--- App/ConvosScreen/ConvoPlayer/index.tsx
+++ App/ConvosScreen/ConvoPlayer/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, Button } from "react-native";
 import TrackPlayer from 'react-native-track-player';
-import {useTrackPlayerProgress, usePlaybackState, useTrackPlayerEvents, TrackPlayerEvents} from 'react-native-track-player';
+import { useTrackPlayerProgress, usePlaybackState, useTrackPlayerEvents, TrackPlayerEvents } from 'react-native-track-player';
 import Slider from '@react-native-community/slider';
 
 export default function ConvoPlayer() {
@@ -10,55 +10,59 @@ export default function ConvoPlayer() {
     const [seekingInProgress, setSeekingInProgress] = useState(false);
     const [sliderValue, setSliderValue] = useState(0);
     const playbackState = usePlaybackState();
-    const trackPlayerProgress = useTrackPlayerProgress(100);           
+    const trackPlayerProgress = useTrackPlayerProgress(100);
 
     useEffect(() => {
-        initializeTrackPlayer().then(()=>{
+        initializeTrackPlayer().then(() => {
             setIsPlayerInitialized(true);
         });
-    }, []);    
+    }, []);
 
-    useEffect(()=>{
-        if(!seekingInProgress)
+    useEffect(() => {
+        if (!seekingInProgress)
             setSliderValue(trackPlayerProgress.position)
     }, [trackPlayerProgress.position]);
 
     useTrackPlayerEvents([TrackPlayerEvents.PLAYBACK_STATE], (event) => {
         if (event.state === TrackPlayer.STATE_PLAYING) {
-          TrackPlayer.play();
+            if (trackPlayerProgress.position >= trackPlayerProgress.duration * .99) {
+                TrackPlayer.seekTo(0);
+            }
+            TrackPlayer.play();
         } else {
-          TrackPlayer.pause();
-        }
-      });
-
-    const onPlayPauseButtonPressed= ()=>{
-        if(playbackState === TrackPlayer.STATE_PLAYING){
             TrackPlayer.pause();
         }
-        else{
-            if(trackPlayerProgress.position === trackPlayerProgress.duration){
+    });
+
+    const onPlayPauseButtonPressed = () => {
+        if (playbackState === TrackPlayer.STATE_PLAYING) {
+            TrackPlayer.pause();
+        }
+        else {
+            console.log("Play/pause. Position: ", trackPlayerProgress.position, " duration: ", trackPlayerProgress.duration * .99);
+            if (trackPlayerProgress.position >= trackPlayerProgress.duration * .99) {
                 TrackPlayer.seekTo(0);
-            }            
+            }
             TrackPlayer.play();
         }
     }
 
-    const setSlidingCompleteVal = (sliderVal: number)=>{
+    const setSlidingCompleteVal = (sliderVal: number) => {
         TrackPlayer.seekTo(sliderVal);
         setSeekingInProgress(false);
     }
 
     return (
         <SafeAreaView style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center', paddingHorizontal: 20 }}>
-            <Slider   
-                onSlidingStart={()=>{setSeekingInProgress(true)}}             
-                onSlidingComplete={(val)=>{setSlidingCompleteVal(val)}}                
+            <Slider
+                onSlidingStart={() => { setSeekingInProgress(true) }}
+                onSlidingComplete={(val) => { setSlidingCompleteVal(val) }}
                 value={sliderValue}
                 maximumValue={trackPlayerProgress.duration}
             />
             <Text>Track Position: {trackPlayerProgress.position}</Text>
             <Text>Track Duration: {trackPlayerProgress.duration}</Text>
-            <Button title={playbackState === TrackPlayer.STATE_PLAYING ? "Pause": "Play"} onPress={() => {onPlayPauseButtonPressed()}} disabled={!isPlayerInitialized}/>
+            <Button title={playbackState === TrackPlayer.STATE_PLAYING ? "Pause" : "Play"} onPress={() => { onPlayPauseButtonPressed() }} disabled={!isPlayerInitialized} />
         </SafeAreaView>
 
     )
@@ -76,16 +80,14 @@ async function initializeTrackPlayer() { //TODO: Take convo details as param
         compactCapabilities: [
             TrackPlayer.CAPABILITY_PLAY,
             TrackPlayer.CAPABILITY_PAUSE
-          ]
+        ]
     })
     await TrackPlayer.add({ //TODO: Update this
         id: 'songID1',
         url: 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',
-        type: 'default',
-        title: 'songTitle1',
-        album: 'album1',
-        artist: 'artist1',
+        title: 'Convo',
+        artist: 'SpeakUp',
         artwork: 'https://picsum.photos/300'
     });
-    console.log("ConvoPlayer::InitializeTrackPlayer. Initialized");    
+    console.log("ConvoPlayer::InitializeTrackPlayer. Initialized");
 }
