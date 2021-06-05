e6a1fb0ad98d60c129b1a7fbc5429b3ca4f70aa6
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-05 13:39:45 -0700

    Starting to integerate auth0'

--- App/LoginScreen/index.tsx
+++ App/LoginScreen/index.tsx
@@ -1,23 +1,48 @@
-import React, {useState} from 'react'
-import { View, StyleSheet, Button, Text, TextInput} from 'react-native';
+import React, { useState } from 'react'
+import { View, StyleSheet, Button, Text, TextInput } from 'react-native';
+import Auth0 from 'react-native-auth0'
+const auth0 = new Auth0({ domain: 'letsusespeakup.us.auth0.com', clientId: 'SIaSdbWJmdIj0MnR5hHFSaGHKlVfgzCT' });
 
-export default function LoginScreen(props: any){    
+export default function LoginScreen(props: any) {
+    const [accessToken, setAccessToken] = useState(null);
+
+    const login = () => {
+        console.log("LoginScreen. login pressed");
+        //TODO
+    }
+
+    const logout = () => {
+        console.log("Login screen. logout pressed");
+        //TODO
+    }
+    return (
+
+        <View style={styles.container}>
+            <Text>Logged In: {accessToken != null ? "True" : "False"}</Text>
+            <Button title={'Login'} onPress={login}> </Button>
+            <Button title={'Logout'} onPress={logout}></Button>
+        </View>
+    )
+}
+
+function OldLoginScreen(props: any) {
     const [tempPhoneNumber, setTempPhoneNumber] = useState('')
 
-    return(
-        <View style={styles.container}>                        
+    return (
+        <View style={styles.container}>
             <Text>Please enter your phone number</Text>
-            <TextInput placeholder="My Phone Number" onChangeText={text=>setTempPhoneNumber(text)} 
-            autoFocus={true} style={{borderWidth: 1, height: 50, width: 200}} maxLength={15}></TextInput>            
-            <Button title={"Confirm"} onPress={()=>{props.onSetPhoneNumber(tempPhoneNumber)}}></Button>               
+            <TextInput placeholder="My Phone Number" onChangeText={text => setTempPhoneNumber(text)}
+                autoFocus={true} style={{ borderWidth: 1, height: 50, width: 200 }} maxLength={15}></TextInput>
+            <Button title={"Confirm"} onPress={() => { props.onSetPhoneNumber(tempPhoneNumber) }}></Button>
         </View>
     );
 }
 
+
 const styles = StyleSheet.create({
-    container:{
-        flex: 1, 
-        justifyContent: 'center', 
+    container: {
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     }
 })
