967dcae4b5d5850a5c6b0a1a32c04a381c714779
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-14 22:03:56 -0700

    Implemented second screen as well as the general component for it.

---
+++ App/LoginScreen/OnboardingScreen/DoubleBlurbScreen/index.tsx
@@ -0,0 +1,64 @@
+import React, {useRef} from 'react'
+import {SafeAreaView, View, StyleSheet, Text, Image, Animated, Button} from 'react-native';
+import {Constants} from '../../../Graphics/index'
+
+export default function DoubleBlurbScreen(props: {topBlurbText: string, bottomBlurbText: string, onNextPressed: ()=>void, onBackPressed?: ()=>void}){
+    const blurbTextAnimation = useRef(new Animated.Value(0)).current
+    React.useEffect(() => {
+        Animated.timing(
+          blurbTextAnimation, {
+            toValue: 1,
+            duration: 2000,
+            useNativeDriver: true
+          }).start();
+      }, [blurbTextAnimation])
+      
+
+    return(
+        <SafeAreaView style={styles.flexContainer}>
+            <View style={styles.topBlurbContainer}>
+                <Text style={{fontFamily: Constants.fontFamily, fontSize: Constants.blurbFontSize, fontWeight: 'bold', marginTop: 20}}>{props.topBlurbText}</Text>
+            </View>
+            <View style={{borderBottomWidth: 2, width: '60%'}}/>
+            <Animated.View style={{display: 'flex', flex: 1, width: '100%', 
+                paddingHorizontal: Constants.paddingHorizontal, opacity: blurbTextAnimation}}>
+                <Text style={{fontFamily: Constants.fontFamily, fontSize: Constants.blurbFontSize, fontWeight: 'bold', marginTop: 20}}>{props.bottomBlurbText}</Text>
+            </Animated.View>
+            <View style={(props.onBackPressed == null) ? styles.oneButtonContainer: styles.twoButtonContainer}>
+                {(props.onBackPressed != null) && <Button title='Back' onPress={props.onBackPressed}/>}
+                <Button title='Next' onPress={props.onNextPressed}/>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    flexContainer: {
+        display: 'flex',
+        flex: 1,        
+        alignItems: 'center',        
+        justifyContent: 'center'
+    },    
+    topBlurbContainer: {
+        display: 'flex',
+        flex: 1.5,    
+        width: '100%',
+        paddingHorizontal: Constants.paddingHorizontal,
+        justifyContent: 'flex-end',
+        paddingBottom: Constants.paddingVertical
+    },
+    twoButtonContainer: {
+       display: 'flex',
+       flexDirection: 'row',
+       alignItems: 'flex-start',
+       justifyContent: 'space-between',
+       paddingHorizontal: Constants.paddingHorizontal,
+       width: '100%'
+    },
+    oneButtonContainer: {
+        display: 'flex',       
+       alignItems: 'flex-end',
+       paddingHorizontal: Constants.paddingHorizontal,
+       width: '100%',       
+    }
+})
--- App/LoginScreen/OnboardingScreen/index.tsx
+++ App/LoginScreen/OnboardingScreen/index.tsx
@@ -1,16 +1,41 @@
 import ImageAndTextBlurbScreen from './ImageAndTextBlurbScreen'
+import DoubleBlurbScreen from './DoubleBlurbScreen';
 import React, {useState } from 'react'
 import {Text} from 'react-native'
 
 
-export default function OnboardingScreen(props: any){
-    const [currentScreen, setCurrentScreen] = useState(); //TODO
+enum Screens{Welcome = 1, SpeakupIsAnApp = 2, InAlpha = 10}; //TODO
+export default function OnboardingScreen(props: {onOnboardingComplete: ()=>void}){
+    const [currentScreen, setCurrentScreen] = useState(Screens.Welcome);
 
-    return(
-        <ImageAndTextBlurbScreen imageSource={welcomeScreenImage} blurbText={welcomeScreenText}
-            onNextPressed={()=>{console.log("next pressed")}}/>         //TODO
-    )
+    const onNextPressed = ()=>{
+        if(currentScreen === Screens.InAlpha){
+            props.onOnboardingComplete();            
+        }
+        else{
+            setCurrentScreen(currentScreen + 1);
+        }        
+    }
+    const onBackPressed = ()=>{
+        setCurrentScreen(currentScreen -1);
+    }
+
+    switch(currentScreen){
+        case Screens.Welcome: {
+            return <ImageAndTextBlurbScreen imageSource={welcomeScreenImage} blurbText={welcomeScreenText}
+                onNextPressed={onNextPressed}/>         
+        }
+        case Screens.SpeakupIsAnApp: {
+            return <DoubleBlurbScreen topBlurbText={speakupIsAnAppTopText} bottomBlurbText={speakupIsAnAppBottomText}
+                onNextPressed={onNextPressed} onBackPressed={onBackPressed}/>
+        }
+        default: return <ImageAndTextBlurbScreen imageSource={welcomeScreenImage} blurbText={welcomeScreenText} //TODO
+        onNextPressed={onNextPressed}/>
+    }    
 }
 
 const welcomeScreenImage = require('../../Graphics/streamline-being-a-vip-social-media-1000x1000.png')
 const welcomeScreenText = 'Welcome to Speakup.'
+
+const speakupIsAnAppTopText = 'Speakup is an app that lets you save and share your best conversations.'
+const speakupIsAnAppBottomText = `Here's how it works.`
