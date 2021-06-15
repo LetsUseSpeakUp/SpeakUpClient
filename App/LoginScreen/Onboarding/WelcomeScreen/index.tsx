6048a11ad9b39153e1ccc23bff68a9cfdb896f22
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-14 20:28:04 -0700

    Got fade in working.

--- App/LoginScreen/Onboarding/WelcomeScreen/index.tsx
+++ App/LoginScreen/Onboarding/WelcomeScreen/index.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import {SafeAreaView, View, StyleSheet, Text, Image} from 'react-native';
+import React, {useRef} from 'react'
+import {SafeAreaView, View, StyleSheet, Text, Image, Animated} from 'react-native';
 import {Constants} from '../../../Graphics/index'
 
 export default function WelcomeScreen(){
+    const fadeAnim = useRef(new Animated.Value(0)).current
+    React.useEffect(() => {
+        Animated.timing(
+          fadeAnim, {
+            toValue: 1,
+            duration: 2000,
+            useNativeDriver: true
+          }).start();
+      }, [fadeAnim])
+
     return(
         <SafeAreaView style={styles.flexContainer}>
             <View style={styles.imageHolder}>
                 <Image source={require('../../../Graphics/streamline-being-a-vip-social-media-1000x1000.png')}
-                    resizeMode='contain' style={{height: '90%'}}/>
+                    resizeMode='contain' style={{height: '70%', marginTop: '20%'}}/>
             </View>
             <View style={{borderBottomWidth: 2, width: '60%'}}/>
-            <View style={styles.textHolder}>
+            <Animated.View style={{display: 'flex', flex: 1, width: '100%', 
+                paddingHorizontal: Constants.paddingHorizontal, opacity: fadeAnim}}>
                 <Text style={{fontFamily: Constants.fontFamily, fontSize: Constants.blurbFontSize, fontWeight: 'bold', marginTop: 20}}>Welcome to Speakup.</Text>
-            </View>
+            </Animated.View>
         </SafeAreaView>
     )
 }
@@ -34,6 +45,6 @@ const styles = StyleSheet.create({
         display: 'flex',
         flex: 1,    
         width: '100%',
-        paddingHorizontal: Constants.paddingHorizontal     
+        paddingHorizontal: Constants.paddingHorizontal       
     }
 })
