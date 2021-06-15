ba90bee90043ffab4d6bb9fbf943c8192c58e1c3
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-14 17:26:44 -0700

    Have an image displaying on splash screen. Need to put the correct one and scale etc.

--- App/SplashScreen/index.tsx
+++ App/SplashScreen/index.tsx
@@ -1,10 +1,12 @@
 import React, {useState, useEffect, useRef, useContext} from 'react'
-import { View, StyleSheet, Text, SafeAreaView, Button} from 'react-native';
+import { View, StyleSheet, Text, SafeAreaView, Image} from 'react-native';
+import {Colors} from '../Graphics'
+
 
 export default function SplashScreen(){
     return (
         <SafeAreaView style={styles.splashImageContainer}>     
-            <Text>box 1</Text>
+            <Image source={require('../Graphics/streamline-icon-make-peace-with-someone@140x140.png')}/>
         </SafeAreaView>
         
     );
@@ -15,6 +17,7 @@ const styles = StyleSheet.create({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        flex: 1  
+        flex: 1,
+        backgroundColor: Colors.backgroundColor
     }
 })
