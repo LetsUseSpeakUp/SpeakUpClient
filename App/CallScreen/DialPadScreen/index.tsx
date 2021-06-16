33cd1f4480d98ef7b6bbbde1b3d4f95b4738707d
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-15 23:53:40 -0700

    handling keyboard dismissal correctly for call screen. Need to style it still.

--- App/CallScreen/DialPadScreen/index.tsx
+++ App/CallScreen/DialPadScreen/index.tsx
@@ -1,28 +1,101 @@
-import React, { useState } from 'react'
-import { ScrollView, View, StyleSheet, Button, Text, TextInput } from 'react-native';
+import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, Animated, ScrollView, Image } from 'react-native'
+import { Constants, Colors, SpeakupTextInput, PrimaryButton } from '../../Graphics';
 
-export default function DialPadScreen(props: any) {
-    const [receiverPhoneNumber, setReceiverPhoneNumber] = useState('')
+export default function DialPadScreen(props: { onCallPlaced: (receiverNumber: string) => void }) {
+    const [receiverNumber, setReceiverNumber] = React.useState('');
+    const callEnabled = isPhoneNumberValid(receiverNumber);
+    const fadeInAnimation = React.useRef(new Animated.Value(0)).current
+    React.useEffect(() => {
+        Animated.timing(
+            fadeInAnimation, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true
+        }).start();
+    }, [fadeInAnimation])
+
+    const nextPressed = () => {
+        // props.onCallPlaced('+1' + receiverNumber);
+    }
 
     return (
-        <ScrollView scrollEnabled={false}>
-            <View style={styles.container}>
-                {/* <Text>Your phone number: {props.userPhoneNumber}</Text> */}
-                <Text>Enter number to call</Text>
-                <TextInput placeholder="Number to call" onChangeText={text => setReceiverPhoneNumber('+1' + text)}
-                    style={{ borderWidth: 1, height: 50, width: 200 }} keyboardType='phone-pad'></TextInput>
-                <Button title={"Call"} onPress={() => { props.onCallPlaced(receiverPhoneNumber) }}></Button>
-            </View>
-        </ScrollView>
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView scrollEnabled={false} style={{ ...styles.flexContainer}} keyboardShouldPersistTaps='handled'>
+                <View style={styles.contentContainer}>
+                    <View style={styles.titleTextContainer}>
+                        <Text style={{
+                            fontFamily: Constants.fontFamily, fontSize: Constants.majorTitleFontSize,
+                            color: Colors.headingTextColor
+                        }}>Place a Call</Text>
+                    </View>
+                    <Animated.View style={{ ...styles.phoneNumberContainer, opacity: fadeInAnimation }}>
+                        <SpeakupTextInput placeholderText={'Phone Number'} onChangeText={(text) => { setReceiverNumber(text) }} autoFocus={true}
+                            keyboardType='number-pad' onSubmitEditing={() => {
+                                if (callEnabled) nextPressed();
+                            }} />
+                    </Animated.View>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton text={'Call'} disabled={!callEnabled} onPress={nextPressed} />
+                    </View>
+                </View>
+                <View style={{...styles.imageHolder}}>
+                    <Image source={require('../../Graphics/streamline-icon-logged-in@1000x1000.png')}
+                        resizeMode='contain' style={{ height: '70%', marginTop: '20%' }} />
+                    </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
 
-    )
+const isPhoneNumberValid = (phoneNumber: string) => {
+    if (phoneNumber.length !== 10) return false;
+    for (let i = 0; i < phoneNumber.length; i++) {
+        const parsedFloat = parseFloat(phoneNumber[i]);
+        if (isNaN(parsedFloat)) return false;
+        if (!isFinite(parsedFloat)) return false;
+        if (parsedFloat < 0) return false;
+    }
+    return true;
 }
 
 const styles = StyleSheet.create({
-    container: {
+    flexContainer: {
+        display: 'flex',
+        backgroundColor: Colors.backgroundColor,
+        flex: 1
+    },
+    contentContainer: {
+        display: 'flex',
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        marginTop: 300
+    },
+    titleTextContainer: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: Constants.paddingTop,
+    },
+    phoneNumberContainer: {
+        paddingTop: Constants.paddingTop,
+        width: '50%'
+    },
+    buttonContainer: {
+        display: 'flex',
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: Constants.paddingTop,
+        paddingBottom: Constants.paddingBottom,
+        paddingHorizontal: Constants.paddingHorizontal,
+        zIndex: 100
+    },
+    imageHolder: {
+        display: 'flex',    
+        height: '200%',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
