9f33fa2b836b87183d59f54a6f4ee3d7fd74f16c
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-16 23:16:45 -0700

    Put in a logout page. It's not great but it'll be sufficient.

--- App/LogoutScreen/index.tsx
+++ App/LogoutScreen/index.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
 import { View, TextInput, Text, Button, StyleSheet } from 'react-native'
 import { _getAuthObject } from '../AuthLogic'
+import {Colors, PrimaryButton} from '../Graphics'
 
 export default function EnterSMSCodeScreen({ route }: any) {
 
     return (
         <View style={styles.container}>
-            <Button title={"Logout"} onPress={() => { route.params.logout() }}></Button>
+            {/* <Button title={"Logout"} onPress={() => { route.params.logout() }}></Button> */}
+            <PrimaryButton text={'Logout'} onPress={() => { route.params.logout() }}/>
         </View>
     );
 }
@@ -15,6 +17,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: Colors.backgroundColor
     }
 })
