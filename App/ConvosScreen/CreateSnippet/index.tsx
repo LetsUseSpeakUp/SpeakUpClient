b4299f060c75771bc53ec2cdadb6c0d7396cbb57
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-17 19:03:02 -0700

    Snippet maker is working and so is sharing! We are almost done with the MVP. Just need to do callkit now.

--- App/ConvosScreen/CreateSnippet/index.tsx
+++ App/ConvosScreen/CreateSnippet/index.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native'
+import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity, ActivityIndicator, Share } from 'react-native'
 import { SpeakupTextInput, Colors, Constants, PrimaryButton } from '../../Graphics'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import {generateSnippetLink} from '../../ConvosData/ConvosManager'
 
 export default function CreateSnippet({ route, navigation }: any) {
     const userFirstName = route.params.userFirstName;
     const [snippetTitle, setSnippetTitle] = React.useState('Snippet from ' + userFirstName);
+    const [isLoading, setIsLoading] = React.useState(false); //TODO
+    const [snippetURL, setSnippetURL] = React.useState('');
 
     const convoId = route.params.convoId;
     const snippetStart = route.params.snippetStart;
     const snippetEnd = route.params.snippetEnd;
 
-    const onCreatePressed = () => {
-        //TODO
+    const onSharePressed = async () => {
+        try{
+            let messageToShare = snippetURL;
+            if(snippetURL.length === 0){
+                setIsLoading(true);
+                const createdSnippetLink: string = await generateSnippetLink(convoId, snippetStart, snippetEnd, snippetTitle);
+                setIsLoading(false);
+                messageToShare = createdSnippetLink;
+                setSnippetURL(createdSnippetLink);
+            }
+            const shareResult = await Share.share({message: messageToShare}); //TODO
+            console.log("CreateSnippet::onSharePressed. ShareResult: ", shareResult);
+        }
+        catch(error){
+            console.log("ERROR -- CreateSnippet::onSharePressed: ", error);
+        }
     }
 
     const onClosePressed = () => {
@@ -34,9 +51,10 @@ export default function CreateSnippet({ route, navigation }: any) {
                 <View style={styles.textInputContainer}>
                     <SpeakupTextInput defaultValue={'Snippet from ' + userFirstName} onChangeText={(newText) => setSnippetTitle(newText)} autoFocus={true} />
                 </View>                
-                <View style={styles.createButtonContainer}>
-                    <PrimaryButton text={'Create'} onPress={onCreatePressed} disabled={snippetTitle.length === 0}/>
+                <View style={styles.shareButtonContainer}>
+                    <PrimaryButton text={'Share'} onPress={onSharePressed} disabled={snippetTitle.length === 0}/>
                 </View>
+                <ActivityIndicator animating={isLoading}/>
             </View>
         </SafeAreaView>
     )
@@ -59,7 +77,7 @@ const styles = StyleSheet.create({
     textInputContainer: {
         width: '80%'
     },
-    createButtonContainer: {
+    shareButtonContainer: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center'
