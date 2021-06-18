a41e504dcf23020c683c2f8088a49ae52fa3d52b
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-17 18:27:43 -0700

    Styled everything to perfection. Now to wire up the methods.

--- App/ConvosScreen/CreateSnippet/index.tsx
+++ App/ConvosScreen/CreateSnippet/index.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
-import {Text, View, StyleSheet, SafeAreaView, TouchableOpacity} from 'react-native'
-import {SpeakupTextInput, Colors, Constants, PrimaryButton} from '../../Graphics'
+import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native'
+import { SpeakupTextInput, Colors, Constants, PrimaryButton } from '../../Graphics'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
-export default function CreateSnippet({route, navigation}: any){
+export default function CreateSnippet({ route, navigation }: any) {
     const [snippetTitle, setSnippetTitle] = React.useState('Snippet from TODO');
 
-    const onCreatePressed = ()=>{
+    const onCreatePressed = () => {
         //TODO
     }
 
-    const onClosePressed = ()=>{
+    const onClosePressed = () => {
         //TODO
     }
 
-    return(
-    <SafeAreaView style={styles.flexContainer}>
-        <View style={styles.closeIconContainer}>
-            <TouchableOpacity onPress={onClosePressed}>
-                <Icon name='close' size={Constants.closeIconSize}/>
-            </TouchableOpacity>            
-        </View>        
-        <Text>Give your Snippet a name.</Text>
-        <SpeakupTextInput defaultValue={'Snippet from TODO'} onChangeText={(newText)=>setSnippetTitle(newText)} autoFocus={true}/>
-        <View style={styles.createButtonContainer}>
-            <PrimaryButton text={'Create'} onPress={onCreatePressed}/>
-        </View>        
-    </SafeAreaView>
+    return (
+        <SafeAreaView style={styles.flexContainer}>
+            <View style={styles.closeIconContainer}>
+                <TouchableOpacity onPress={onClosePressed}>
+                    <Icon name='close' size={Constants.closeIconSize} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.contentContainer}>
+                <Text style={{color: Colors.headingTextColor, fontSize: Constants.majorTitleFontSize, fontFamily: Constants.fontFamily}}>
+                    Give your Snippet a name.
+                </Text>
+                <View style={styles.textInputContainer}>
+                    <SpeakupTextInput defaultValue={'Snippet from TODO'} onChangeText={(newText) => setSnippetTitle(newText)} autoFocus={true} />
+                </View>                
+                <View style={styles.createButtonContainer}>
+                    <PrimaryButton text={'Create'} onPress={onCreatePressed} disabled={snippetTitle.length === 0}/>
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
 
@@ -35,17 +41,27 @@ const styles = StyleSheet.create({
         display: 'flex',
         flex: 1,
         backgroundColor: Colors.backgroundColor,
-        paddingTop: Constants.paddingTop        
+        paddingTop: Constants.paddingTop
     },
     closeIconContainer: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'flex-end',
-        paddingRight: Constants.paddingTop
+        paddingRight: Constants.paddingTop,
+        flex: .1        
+    },
+    textInputContainer: {
+        width: '80%'
     },
     createButtonContainer: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    contentContainer: {
+        display: 'flex',        
+        justifyContent: 'space-between',
+        alignItems: 'center',        
+        flex: .4,    
     }
 })
